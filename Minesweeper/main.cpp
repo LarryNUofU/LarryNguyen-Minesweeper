eba997607ccd759248e493de1c7f53ebb0d7d2a1
Larry Nguyen <lepyzoom@live.com>
2018-11-01 19:49:49 -0600

    Changed variables coming into main menu loop as references so that the debug will run.

--- Minesweeper/main.cpp
+++ Minesweeper/main.cpp
@@ -2,24 +2,60 @@
 #include <Windows.h>
 
 bool inMainMenu = true;
-sf::RenderWindow  window(sf::VideoMode(1280, 720), "SFML works!");
-
+ 
 
 //The loop that runs at the main menu
-void mainMenuLoop(sf::Event event)
+void mainMenuLoop(sf::RenderWindow &window, sf::Event  &event, sf::Clock *clock)
 {
-	sf::CircleShape circle(100.f);
+	//button
+	sf::RectangleShape easyButton(sf::Vector2f(100, 100));
+	sf::Font font;
+	if (!font.loadFromFile("Fonts/Roboto-Black.ttf"))
+	{
+		// error...
+		easyButton.setFillColor(sf::Color::Cyan);
+	}
+	//////logic to make text go center of button///////
+	sf::Text text;
+	text.setFont(font);
+
+	text.setString("3");
+
+	text.setCharacterSize(48); // in pixels, not points!
+
+	text.setFillColor(sf::Color::Red);
+
+	text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+	sf::FloatRect textBounds(text.getLocalBounds());
+	sf::Vector2f squareSize(easyButton.getSize());
+
+	//line below thanks to Mario from StackOverflow!
+	text.setOrigin((textBounds.width - squareSize.x) / 2 + textBounds.left, (textBounds.height - squareSize.y) / 2 + textBounds.top);
+
+	text.setPosition(easyButton.getPosition());
+
+
+
+	easyButton.setPosition(sf::Vector2f(0.f, 0.f));
+
+
+
 	while (window.pollEvent(event))
 	{
 		if (event.type == sf::Event::Closed)
 			window.close();
 		if (event.type == sf::Event::MouseButtonReleased)
 		{
-			sf::FloatRect fr = circle.getGlobalBounds();
+			sf::FloatRect fr = easyButton.getGlobalBounds();
 			sf::Vector2i mouseCoords = sf::Mouse::getPosition(window);
 			sf::Vector2f worldPos = window.mapPixelToCoords(mouseCoords);
 			if (fr.contains(worldPos.x, worldPos.y))
+			{
 				inMainMenu = false;
+				clock->restart();
+			}
+				
 		}
 
 	}
@@ -28,7 +64,7 @@ void mainMenuLoop(sf::Event event)
 
 
 	window.clear();
-	window.draw(circle);
+	window.draw(easyButton);
 	window.display();
 
 
@@ -45,7 +81,8 @@ int main()
 	FreeConsole();
 	
 
-
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+	
 	sf::CircleShape shape(100.f);
 	sf::RectangleShape square(sf::Vector2f(100, 100));
 	square.setPosition(200.f, 0.f);
@@ -72,7 +109,7 @@ int main()
 
 		if (inMainMenu)
 		{
-			mainMenuLoop(event);
+			mainMenuLoop(window, event, &clock);
 		}
 		else
 		{
