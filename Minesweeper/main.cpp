53375a22f3d36fb27dc71fa42e78b32165182140
Larry Nguyen <lepyzoom@live.com>
2018-11-01 18:45:12 -0600

    Added logic to transition from main menu loop to gameplay loop.

--- Minesweeper/main.cpp
+++ Minesweeper/main.cpp
@@ -1,11 +1,51 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
+
+bool inMainMenu = true;
+sf::RenderWindow  window(sf::VideoMode(1280, 720), "SFML works!");
+
+
+//The loop that runs at the main menu
+void mainMenuLoop(sf::Event event)
+{
+	sf::CircleShape circle(100.f);
+	while (window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			window.close();
+		if (event.type == sf::Event::MouseButtonReleased)
+		{
+			sf::FloatRect fr = circle.getGlobalBounds();
+			sf::Vector2i mouseCoords = sf::Mouse::getPosition(window);
+			sf::Vector2f worldPos = window.mapPixelToCoords(mouseCoords);
+			if (fr.contains(worldPos.x, worldPos.y))
+				inMainMenu = false;
+		}
+
+	}
+	
+
+
+
+	window.clear();
+	window.draw(circle);
+	window.display();
+
+
+
+}
+
+
+
+
+
+
 int main()
 {
 	FreeConsole();
+	
 
 
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
 	sf::CircleShape shape(100.f);
 	sf::RectangleShape square(sf::Vector2f(100, 100));
 	square.setPosition(200.f, 0.f);
@@ -28,100 +68,112 @@ int main()
 	while (window.isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event))
+
+
+		if (inMainMenu)
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-			if (event.type == sf::Event::MouseButtonReleased)
+			mainMenuLoop(event);
+		}
+		else
+		{
+			//in this else statement, we are in the gameplay loop
+
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+				if (event.type == sf::Event::MouseButtonReleased)
+				{
+					sf::FloatRect fr = square.getGlobalBounds();
+					sf::Vector2i mouseCoords = sf::Mouse::getPosition(window);
+					sf::Vector2f worldPos = window.mapPixelToCoords(mouseCoords);
+					if (fr.contains(worldPos.x, worldPos.y) && validClick)
+						square.setFillColor(sf::Color::White);
+
+					validClick = true;
+				}
+
+			}
+
+
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && validClick)
 			{
 				sf::FloatRect fr = square.getGlobalBounds();
 				sf::Vector2i mouseCoords = sf::Mouse::getPosition(window);
 				sf::Vector2f worldPos = window.mapPixelToCoords(mouseCoords);
-				if (fr.contains(worldPos.x, worldPos.y) && validClick)
-					square.setFillColor(sf::Color::White);
+				if (fr.contains(worldPos.x, worldPos.y))
+				{
+					texture.loadFromFile("Textures/texture-2.png");
+					square.setTexture(&texture);
+				}
+				else
+				{
+					texture.loadFromFile("Textures/texture-3.png");
+					square.setTexture(&texture);
+					validClick = false;
+				}
 
-				validClick = true;
 			}
 
-		}
 
 
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && validClick)
-		{
-			sf::FloatRect fr = square.getGlobalBounds();
-			sf::Vector2i mouseCoords = sf::Mouse::getPosition(window);
-			sf::Vector2f worldPos = window.mapPixelToCoords(mouseCoords);
-			if (fr.contains(worldPos.x, worldPos.y))
+
+			sf::FloatRect recBounds = square.getGlobalBounds();
+			sf::Vector2f vec = square.getPosition();
+			sf::Vector2f circleVec = sf::Vector2f(vec.x + recBounds.width / 2, vec.y + recBounds.height / 2);
+			//shape.setPosition(circleVec);
+
+
+			sf::Font font;
+			if (!font.loadFromFile("Fonts/Roboto-Black.ttf"))
 			{
-				texture.loadFromFile("Textures/texture-2.png");
-				square.setTexture(&texture);
+				// error...
+				square.setFillColor(sf::Color::Cyan);
 			}
-			else
-			{
-				texture.loadFromFile("Textures/texture-3.png");
-				square.setTexture(&texture);
-				validClick = false;
-			}
-			
+
+			sf::Text text;
+
+			// select the font
+			text.setFont(font); // font is a sf::Font
+
+			// set the string to display
+			text.setString("3");
+
+			// set the character size
+			text.setCharacterSize(48); // in pixels, not points!
+
+			// set the color
+			text.setFillColor(sf::Color::Red);
+
+			// set the text style
+			text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+			sf::FloatRect textBounds(text.getLocalBounds());
+			sf::Vector2f squareSize(square.getSize());
+
+			//line below thanks to Mario from StackOverflow!
+			text.setOrigin((textBounds.width - squareSize.x) / 2 + textBounds.left, (textBounds.height - squareSize.y) / 2 + textBounds.top);
+
+			text.setPosition(square.getPosition());
+
+
+
+			shape.setPosition(sf::Vector2f(0.f, 0.f));
+
+
+			sf::Time elapsed = clock.restart();
+
+			square.move(sf::Vector2f(20.f * elapsed.asSeconds(), 0.f));
+
+			window.clear();
+
+			window.draw(square);
+			window.draw(text);
+			window.draw(shape);
+
+			window.display();
 		}
 
-		
-
-
-		sf::FloatRect recBounds = square.getGlobalBounds();
-		sf::Vector2f vec = square.getPosition();
-		sf::Vector2f circleVec = sf::Vector2f(vec.x + recBounds.width / 2, vec.y + recBounds.height / 2);
-		//shape.setPosition(circleVec);
-
-
-		sf::Font font;
-		if (!font.loadFromFile("Fonts/Roboto-Black.ttf"))
-		{
-			// error...
-			square.setFillColor(sf::Color::Cyan);
-		}
-
-		sf::Text text;
-
-		// select the font
-		text.setFont(font); // font is a sf::Font
-
-		// set the string to display
-		text.setString("3");
-
-		// set the character size
-		text.setCharacterSize(48); // in pixels, not points!
-
-		// set the color
-		text.setFillColor(sf::Color::Red);
-
-		// set the text style
-		text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-
-		sf::FloatRect textBounds(text.getLocalBounds());
-		sf::Vector2f squareSize(square.getSize());
-
-		//line below thanks to Mario from StackOverflow!
-		text.setOrigin((textBounds.width - squareSize.x) / 2 + textBounds.left, (textBounds.height - squareSize.y) / 2 + textBounds.top);
-
-		text.setPosition(square.getPosition());
-
-
-
-		shape.setPosition(sf::Vector2f(0.f,0.f));
-
-
-		sf::Time elapsed = clock.restart();
-
-		square.move(sf::Vector2f(20.f * elapsed.asSeconds(), 0.f));
-
-		window.clear();
-		
-		window.draw(square);
-		window.draw(text);
-		window.draw(shape);
-		
-		window.display();
 	}
 
 	return 0;
